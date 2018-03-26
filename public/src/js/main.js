e1b60b350651992cddae736298070f7747dd3b95
Bayley <cbayley@proforma.com>
2018-03-26 14:30:07 -0400

    Stamps - And then angular

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,4 +1,33 @@
 /* global angular */
+/* global stampit */
+
+const createStamp = stampit.default;
+
+const Team = createStamp({
+    props: {
+        name: 'A Default Team',
+        score: 0
+    },
+    init({ name = this.name }) {
+        this.name = name;
+    },
+    methods: {
+        setName(newName) {
+            if (newName) {
+                this.name = newName;
+            }
+        },
+        adjustScore(delta) {
+            this.score += delta;
+        }
+    }
+});
 
 angular
-    .module('scoreBoardApp', []);
+    .module('scoreBoardApp', [])
+    .controller('gameController', ($scope) => {
+        $scope.teams = new Array(2);
+    })
+    .controller('teamController', ($scope) => {
+        $scope.team = Team({});
+    });
